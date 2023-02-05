ecca09c84d2597f82fa61296a928735739c1cbee
Mogbojuloluwa Odesola <mogbojulolua@gmail.com>
2023-02-05 22:26:39 +0100

    Load balancer configuration

--- loadBalancer.tf
+++ loadBalancer.tf
@@ -1,4 +1,4 @@
-resource "aws_lb" "main" {
+resource "aws_lb" "gbojus_lb" {
   name = var.lb_name
   internal = false
   load_balancer_type = "application"
@@ -6,11 +6,11 @@ resource "aws_lb" "main" {
   subnets = [for subnet in aws_subnet.subnets : subnet.id]
 }
 
-resource "aws_lb_target_group" "main" {
+resource "aws_lb_target_group" "gbojus_tg" {
   name = var.tg_name
   port = 80
   protocol = "HTTP"
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.gboju.id
 
   health_check {
     port = 80
@@ -20,22 +20,22 @@ resource "aws_lb_target_group" "main" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "main" {
-  for_each = aws_instance.main
+resource "aws_lb_target_group_attachment" "gboju" {
+  for_each = aws_instance.gboju
 
-  target_group_arn = aws_lb_target_group.main.arn
+  target_group_arn = aws_lb_target_group.gboju.arn
   target_id = each.value.id
   port = 80
 }
 
 resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.main.arn
+  load_balancer_arn = aws_lb.gboju.arn
   port = 80
   protocol = "HTTP"
 
   default_action {
     type = "forward"
-    target_group_arn = aws_lb_target_group.main.arn
+    target_group_arn = aws_lb_target_group.gbojus_tg.arn
   }
 }
 
